a04542c6516eb7c69693471bcd7440bd86b53df4
Beck <physicalc123@gmail.com>
2018-01-19 17:08:40 -0500

    Fixed bugs

--- js/main.js
+++ js/main.js
@@ -18,14 +18,18 @@ $(window).scroll(function() {
     $('svg.scroll-shrinkable').addClass('svg-small');
     lt.changeText(names[0], names[1]);
     lt.changeText(names[2], names[3]);
-    lt.moveText("T", 495.95773, 500);//Text small, centered, and same line
-    lt.moveText("B", 432.95773, 500)
+    lt.moveText("T", 495.95773, 845.70038);//Text small, centered, and same line
+    lt.moveText("B", 432.95773, 845.70038);
+    $('svg.scroll-shrinkable').find("tspan").addClass('l');
+    $('svg.scroll-shrinkable').find("tspan").removeClass('s');
   } else {
     $('nav.scroll-shrinkable').removeClass('nav-small');
     $('svg.scroll-shrinkable').removeClass('svg-small');
     lt.changeText(names[1], names[0]);
     lt.changeText(names[3], names[2]);
     lt.moveText("Teck", 432.95773, 908.80585);
-    lt.moveText("Beck", )
+    lt.moveText("Beck", 432.95773, 796.30585);
+    $('svg.scroll-shrinkable').find("tspan").addClass('s');
+    $('svg.scroll-shrinkable').find("tspan").removeClass('l');
   }
 });
